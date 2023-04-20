83df83f5dcb2236e4aa4daace87638dfe2d25f76
walterjhon <walterjhon7@gmail.com>
2023-04-19 20:10:18 -0500

    Categoria Model

---
+++ src/main/java/com/inventario/model/Categoria.java
@@ -0,0 +1,55 @@
+package com.inventario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoriaId;
+    @Column(name="Codigo")
+    private String codigoCategoria;
+	@Column(name="Nombre")
+    private String nombreCategoria;
+	
+	@Column(name="Descripcion")
+    private String descripcionCategoria;
+
+    public Long getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Long categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(String codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getDescripcionCategoria() {
+        return descripcionCategoria;
+    }
+
+    public void setDescripcionCategoria(String descripcionCategoria) {
+        this.descripcionCategoria = descripcionCategoria;
+    }
+
+}
